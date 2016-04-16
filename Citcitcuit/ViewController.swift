4eaa9e4c42ba4c92e94baad9eb964a22f38348a9
@banumelody <banumelady@gmail.com>
2016-04-16 11:15:18 +0700

    Initial Commit

---
+++ Citcitcuit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Citcitcuit
+//
+//  Created by Banu Antoro on 4/16/16.
+//  Copyright © 2016 Banu Antoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
